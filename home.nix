ee952fb5e66539840ac5156b807d72b3014c8990
Muktazam Hasbi Ashidiqi <hasbeeazam@gmail.com>
2025-04-07 09:17:13 +0700

    add: apply hack to duplicate icons on switch and remove on login

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Home Manager needs a bit of information about you and the
@@ -48,6 +48,7 @@
     gnome-power-manager
     lsof
     ripgrep
+    desktop-file-utils
 
     # multimedia
     mpv
@@ -122,6 +123,11 @@
       bindkey "^[[A" history-substring-search-up
       bindkey "^[OB" history-substring-search-down
       bindkey "^[[B" history-substring-search-down
+        '';
+    profileExtra = lib.mkAfter ''
+      rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+      rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+      ls ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop > ${config.home.homeDirectory}/.cache/current_desktop_files.txt
         '';
   };
 
@@ -200,4 +206,35 @@
   };
 
   home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  home.activation = {
+    linkDesktopApplications = {
+      after = ["writeBoundary" "createXdgUserDirectories"];
+      before = [];
+      data = ''
+        rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+        rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+        mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
+        mkdir -p ${config.home.homeDirectory}/.icons
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
+
+        # Check if the cached desktop files list exists
+        if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
+          current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
+        else
+          current_files=""
+        fi
+
+        # Symlink new desktop entries
+        for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
+          if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
+            ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
+          fi
+        done
+
+        # Update desktop database
+        ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
+      '';
+    };
+  };
 }
